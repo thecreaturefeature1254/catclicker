3cb8aab890e26de405d031dc53d8ef47a569f2af
thecreaturefeature1254 <richterbalch1@gmail.com>
2025-12-03 20:25:48 -0600

    Update CurrencyManager.js

--- CurrencyManager.js
+++ CurrencyManager.js
@@ -1,4 +1,13 @@
-if (window.loaderready) {
-  var localclicks = localStorage.getItem("clicks")
-  var sessionclicks = sessionStorage.getItem("sessionclicks")
-}
+let check = setInterval(() => {
+  if (window.loaderready) {
+    clearInterval(check);
+
+    const localclicks = Number(localStorage.getItem("clicks") || 0);
+    const sessionclicks = Number(sessionStorage.getItem("sessionclicks") || 0);
+    const counter = document.getElementById("num1");
+
+    if (counter) {
+      counter.textContent = localclicks + sessionclicks;
+    }
+  }
+}, 100);
