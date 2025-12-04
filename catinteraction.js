58482fc0598dfca051bf1cfc8c88724f6b2e338f
thecreaturefeature1254 <richterbalch1@gmail.com>
2025-12-03 20:33:40 -0600

    Update catinteraction.js

--- catinteraction.js
+++ catinteraction.js
@@ -1,16 +1,16 @@
 // wait on the slowpoke
 let check = setInterval(() => {
   if (window.loaderready) {
-    clearInterval(check);
-  }
-}, 100); // check every 100ms
-if (window.loaderready) {
-var sessionclicks = localStorage.getItem("sessionclicks")
+    clearInterval(check)
+    var sessionclicks = localStorage.getItem("sessionclicks")
 var multiplier = localStorage.getItem("multiplier")
 var button = document.getElementById("cat")              
-}
+
   button.addEventListener("click", function () {
     let sessionclicknumber = Number(sessionclicks)
     let result = sessionclicknumber + 1 * Number(multiplier)
     localStorage.setItem("sessionclicks", result)
   });
+  }
+  }
+
